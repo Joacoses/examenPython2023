66b3c1ad8e086051ac026de533af14452ebde952
Joan Costa Escrivà <joance3@gmail.com>
2023-03-22 10:51:21 +0100

    Commit_3

--- functions.py
+++ functions.py
@@ -3,13 +3,15 @@ import csv
 f = 'examenPython\examenPython2023\\winequality.csv'
 def read_data(fichero):
     contador = 0
+    dic = {}
     with open(fichero, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             contador += 1
-            for i in 
-            dic = {}
-
-            print(row)
+            for i in row:
+                dic = {tuple(row[i] : )}
+                dic = {'dato'+str(contador): 'John', tuple(row):'values'}
+           
+            print(dic)
 
 res = read_data(f)
