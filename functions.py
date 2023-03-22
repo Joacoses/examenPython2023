3f4c6e11b579cf47bd3e08c4b66bfa27636bd21b
Joan Costa Escrivà <joance3@gmail.com>
2023-03-22 11:17:50 +0100

    Commit_4

--- functions.py
+++ functions.py
@@ -1,17 +1,33 @@
 import csv
 
 f = 'examenPython\examenPython2023\\winequality.csv'
+
 def read_data(fichero):
     contador = 0
-    dic = {}
+    datos = ["type", "fixed acidity", "volatile acidity", "citric acidity", "residual sugar", " chlorides", "free sulfur dioxide", "total sulfur dioxide", "density", "PH", "sulphates", "alcohol"]
+    
+    #leer
     with open(fichero, 'r') as file:
         reader = csv.reader(file)
+
+        #inicializamos contador y transformamos a tupla para tratar mas facilmente
         for row in reader:
             contador += 1
-            for i in row:
-                dic = {tuple(row[i] : )}
-                dic = {'dato'+str(contador): 'John', tuple(row):'values'}
-           
-            print(dic)
+            tuple(row)
+
+            #juntamos los datos del csv con el texto
+            lista_tuplas = list(zip(datos, row))
+            diccionario = dict(lista_tuplas)
+
+            #diccionario final
+            for i in diccionario:
+                dic_final = {"dato"+ str(contador): diccionario}
+            return dic_final
 
 res = read_data(f)
+
+def split(d):
+    print(d)
+
+
+res2 = split(dic_final)
